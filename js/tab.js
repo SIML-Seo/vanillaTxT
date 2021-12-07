d8de838c2fd35dca971b730036e6b9f8d09eb32d
Wonho Seo <swh1182@naver.com>
2021-12-07 16:02:34 +0900

    15%

--- js/tab.js
+++ js/tab.js
@@ -1,13 +1,22 @@
 const TxT = document.getElementById("TxT");
 const tabButton = document.getElementById("tabBtn");
 const tabCon = document.getElementById("tab-container");
+let tabConChild = tabCon.querySelectorAll(":scope > *");
 
 let tabs = [];
+let tab = "";
+
+const STORE_KEY = "TXT";
+
+function saveTab(tabs){
+    localStorage.setItem(STORE_KEY, JSON.stringify(tabs));
+}
 
 function deleteTab(event){
     const div = event.target.parentElement;
     div.remove();
-
+    tabs = tabs.filter((tab) => tab.id !== parseInt(div.id));
+    saveTab(tabs);
 }
 
 function handleTab(newTab){
@@ -15,16 +24,18 @@ function handleTab(newTab){
     div.id = newTab.id;
     const span = document.createElement("span");
     span.innerText = newTab.text;
+    span.id = "tabSpan"
     const button = document.createElement("button");
-    button.innerText = "X";
+    button.innerText= "X";
     button.addEventListener("click", deleteTab);
     div.appendChild(span);
     div.appendChild(button);
     tabCon.appendChild(div);
 }
 
-function clickTab(){
-    const value = TxT.value;
+function clickTabBtn(){
+    const value = "";
+    // const value = TxT.value;
     const newTab = {
         text : value,
         id : Date.now()
@@ -32,6 +43,26 @@ function clickTab(){
     tabs.push(newTab);
     handleTab(newTab);
     TxT.value = "";
+    saveTab(tabs);
 }
 
-tabButton.addEventListener("click", clickTab);
+function clickTab(){
+    console.log("AAAA")
+    let content = document.getElementById("tabSpan").innerText;
+    TxT.value = content;
+
+    // TxT.addEventListener("input", function(event){
+    //     console.log(event.target.value)
+    //     content = event.target.value;
+    // });
+}
+
+function insertCont(){
+    let content = document.getElementById("tabSpan");
+    console.log(TxT.value)
+    content.innerText = TxT.value
+}
+
+tabButton.addEventListener("click", clickTabBtn);
+tabConChild.addEventListener("click", clickTab);
+TxT.addEventListener("input", insertCont);
