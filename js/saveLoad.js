5dad57d924eafd5b4841e84d23c808c2551f49f1
Seo WonHo <34260439+SIML-Seo@users.noreply.github.com>
2021-12-10 00:54:45 +0900

    Add files via upload

--- js/saveLoad.js
+++ js/saveLoad.js
@@ -2,23 +2,25 @@ const saveButton = document.getElementById("saveBtn");
 const loadButton = document.getElementById("loadBtn");
 const saveCon = document.getElementById("save-container");
 
-let flag = 0;
+let dataModifyFlag = [];
 
 function save() {
     let TxTData = tabCon.querySelector(":scope > .selected > textarea").value;
     console.log("Save Data : " + TxTData);
     let title = prompt("저장하기")
-    console.log("COMPLETE SAVE!");
     if(saveChecking(title) === 0){
+        console.log("Failed Save!")
         return alert("이미 저장되어 있는 데이터가 있습니다.")
     }
-    const saveTabData = newTab(TxTData);
+    console.log("COMPLETE SAVE!");
+    const saveTabData = tapInfo(TxTData);
     tabs.push(saveTabData);
     saveTab(saveTabData, title);
     let p = document.createElement("p");
     p.innerText = title;
     p.addEventListener("click", function(){showData(p.innerText)});
     saveCon.appendChild(p);
+    tabLi.querySelector(":scope > .selected").innerText = title;
 }
 
 function saveChecking(title){
@@ -30,7 +32,11 @@ function saveChecking(title){
 }
 
 function indicator(){
-    
+    let TxTData = tabCon.querySelector(":scope > .selected > textarea").value;
+    if(TxTData !== dataModifyFlag){
+        alert("글이 수정되었습니다. 저장하세요.")
+        return 0;
+    }
 }
 
 function clickloadBtn() {
@@ -48,7 +54,11 @@ function loadData(title){
     console.log("TxTData : " + TxTData);
     let savedTab = tabs.find((tab) => tab.text === TxTData);
     console.log("savedTab : " + savedTab);
-    handleTab(savedTab);
+    if(handleTab(savedTab) === "1"){
+        tabLi.querySelector(":scope > .selected").innerText = title;
+        dataModifyFlag = TxTData;
+        console.log("COMPLETE LOAD!")
+    }
 }
 
 
